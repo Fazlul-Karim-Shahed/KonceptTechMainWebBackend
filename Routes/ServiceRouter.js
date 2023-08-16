924a5148ffd13c09c0980fdd94037ff3f7b0aad9
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2023-08-16 23:04:04 +0600

    bug fix

--- Routes/ServiceRouter.js
+++ Routes/ServiceRouter.js
@@ -2,15 +2,15 @@ const checkAdmin = require('../Middlewares/checkAdmin')
 const getOneCourse = require('./RouterFunctions/CourseFunctions/getOneCourse')
 const createService = require('./RouterFunctions/ServiceFunctions/createService')
 const deleteService = require('./RouterFunctions/ServiceFunctions/deleteService')
+const getOneService = require('./RouterFunctions/ServiceFunctions/getOneService')
 const getService = require('./RouterFunctions/ServiceFunctions/getService')
 const updateService = require('./RouterFunctions/ServiceFunctions/updateService')
 
 const router = require('express').Router()
 
 
-
 router.get('/', getService)
-router.get('/:id', getOneCourse)
+router.get('/:id', getOneService)
 router.post('/create', checkAdmin, createService)
 router.put('/update', checkAdmin, updateService)
 router.delete('/delete', checkAdmin, deleteService)
